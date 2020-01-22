ba3637508d9d2f78744915ae4ac46e8b4c764be3
danhrubec <60075606+danhrubec@users.noreply.github.com>
2020-01-22 11:05:42 -0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
+function toggleSideBar()
+{
+    document.getElementById("sidebar").classList.toggle('active');  
+}
